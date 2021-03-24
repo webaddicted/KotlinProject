4300d8715cc9a6d7a51b0da70dbd3b468dba7666
Deepak Sharma <deepaksharma040695@gmail.com>
2021-03-24 10:52:39 +0530

    update version

--- app/src/main/java/com/webaddicted/kotlinproject/view/activity/SplashActivity.kt
+++ app/src/main/java/com/webaddicted/kotlinproject/view/activity/SplashActivity.kt
@@ -16,17 +16,18 @@ import com.webaddicted.kotlinproject.view.base.BaseActivity
 import java.util.*
 
 
-
 /**
  * Created by Deepak Sharma on 01/07/19.
  */
 class SplashActivity : BaseActivity() {
     private lateinit var mBinding: ActivitySplashBinding
+
     companion object {
         val TAG: String = SplashActivity::class.java.simpleName
         fun newIntent(activity: Activity) {
             activity.startActivity(Intent(activity, SplashActivity::class.java))
         }
+
         fun newClearLogin(context: Activity?) {
             val intent = Intent(context, SplashActivity::class.java)
             intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP or Intent.FLAG_ACTIVITY_NEW_TASK)
@@ -42,66 +43,14 @@ class SplashActivity : BaseActivity() {
     override fun initUI(binding: ViewDataBinding) {
         mBinding = binding as ActivitySplashBinding
         init()
-        setNavigationColor(ContextCompat.getColor(context,R.color.app_color))
+        setNavigationColor(ContextCompat.getColor(context, R.color.app_color))
         mBinding.imgLogo.setOnClickListener { init() }
     }
 
     private fun init() {
         val slideAnmimation = AnimationUtils.loadAnimation(this, R.anim.bounce_game)
         mBinding.imgLogo.animation = slideAnmimation
-//        mBinding.imgLogo.setOnClickListener(object : View.OnClickListener {
-//            override fun onClick(p0: View?) {
-//    if (isInternetConnectedtest){
-//        isInternetConnectedtest = false
-//    }else{
-//        isInternetConnectedtest = true
-//    }
-//                showInternetSnackbar(isInternetConnectedtest, mBinding.txtNoInternet)
-//            }
-//        })
         navigateToNext()
-        val myScanner = Scanner(System.`in`)
-        println("Enter the value of N")
-        val n = 5
-
-        val a = generateShuffledArrayOfRange(n)
-        val arrayWithMissingNum = a.subList(0, n - 1)
-        val missingNum = a[n - 1]
-
-        Lg.d(TAG,"Shuffled array = $arrayWithMissingNum")
-        Lg.d(TAG,"Missing number = $missingNum")
-
-        val missingNumberFromAlgo = MySolution(arrayWithMissingNum)
-        Lg.d(TAG,"Missing number from algo = $missingNumberFromAlgo")
-
-        val isAnswerCorrect = missingNumberFromAlgo == missingNum
-        Lg.d(TAG,"Your solution is $isAnswerCorrect")
-    }
-    private fun generateShuffledArrayOfRange(n: Int): List<Int> {
-        val a = (1..n).toList()
-        val temp = a.toMutableList()
-        temp.shuffle()
-        return temp.toList()
-    }
-
-
-    private fun MySolution(arrayWithMissingNum: List<Int>): Int {
-        var sum = 0
-        var idx = -1
-        for (i in arrayWithMissingNum.indices) {
-            if (arrayWithMissingNum[i] === 0) {
-                idx = i
-            } else {
-                sum += arrayWithMissingNum[i]
-            }
-        }
-
-// the total sum of numbers between 1 and arr.length.
-        val total = (arrayWithMissingNum.size + 1) * arrayWithMissingNum.size/ 2
-
-        Lg.d(TAG,"missing number is: " + (total - sum) + " at index " + idx)
-        Lg.d(TAG, "")
-        return 0
     }
 
     /**
